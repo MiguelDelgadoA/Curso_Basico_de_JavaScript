41cdd026e076adc6483431751734ff291b41b067
Midelar <Midelar>
2020-06-05 14:11:15 -0500

    Finalizado los 10 videos

---
+++ funciones.js
@@ -0,0 +1,15 @@
+//Declarativas
+function miFuncion(){
+    return 3;
+}
+
+function sEstudiante(estudiante){
+    console.log(`Hola ${estudiante}`);
+    }
+sEstudiante("Miguel")
+
+//Expresión  tambien conocidas como Funciones ANONIMAS
+
+var miFuncion = function(a,b){
+    return a+b;
+} 
---
+++ variables.js
@@ -0,0 +1,12 @@
+var nombre = "Oscar"
+//declarar 
+var edad;
+//inicializar
+edad = 30
+
+var elementos =["Computadora", "Celular"]
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
